d0ab18af94ca084d63c9de4f15c24d73ff0539d2
Tatu Aalto <aalto.tatu@gmail.com>
2024-03-29 21:48:17 +0200

    basic linting

---
+++ tasks.py
@@ -0,0 +1,14 @@
+from invoke import task
+
+
+@task
+def lint(ctx, fix=False):
+    print("Run mypy:")
+    ctx.run("mypy --exclude .venv .")
+    print("Run black:")
+    ctx.run("black .")
+    print("Run ruff:")
+    ruff_cmd = "ruff check "
+    if fix:
+        ruff_cmd = f"{ruff_cmd} --fix"
+    ctx.run(ruff_cmd)
