d4ca8a53c1a835f9b2cba2ee3105b167255f9acc
Tatu Aalto <aalto.tatu@gmail.com>
2024-03-29 22:56:20 +0200

    Add translation code and file

--- utest/test_translation.py
+++ utest/test_translation.py
@@ -1,2 +1,16 @@
+from pathlib import Path
+
+import robotframework_browser_translation_fi
+
+
 def test_translation():
-    pass
+    lang = robotframework_browser_translation_fi.get_language()
+    expexted_path = (
+        Path(__file__).parent.parent
+        / "robotframework_browser_translation_fi"
+        / "translation.json"
+    )
+    assert lang["language"] == "fi"
+    result_path = Path(lang["path"])
+    assert result_path == expexted_path
+    assert result_path.is_file()
