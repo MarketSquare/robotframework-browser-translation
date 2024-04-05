250c8206332734ff252ae084285290c73940cf7f
Tatu Aalto <aalto.tatu@gmail.com>
2024-04-06 01:55:31 +0300

    chore: lint fixes

--- atest/json_lib.py
+++ atest/json_lib.py
@@ -4,11 +4,13 @@ from pathlib import Path
 from robot.api import logger
 
 
-def compare_translations(file: Path) -> dict:
-    with Path(__file__).parent.parent.joinpath("robotframework_browser_translation_fi", "translation.json").open("r") as file_object:
+def compare_translations(file: Path):
+    with Path(__file__).parent.parent.joinpath(
+        "robotframework_browser_translation_fi", "translation.json"
+    ).open("r") as file_object:
         expected_data = json.load(file_object)
-    expected_keywords = [kw["name"]for kw in expected_data.values()]
-    logger.info(expected_keywords)
+    expected_keywords = [kw["name"] for kw in expected_data.values()]
+    logger.info(f"expected_keywords: {expected_keywords}")
     with file.open("r") as file_object:
         data = json.load(file_object)
     keywords = data["keywords"]
