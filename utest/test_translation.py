3b2fdcf256abf6b7038810250aec552fe26399ec
Tatu Aalto <aalto.tatu@gmail.com>
2024-04-05 22:39:22 +0300

    fix: remove space from translated keyword

--- utest/test_translation.py
+++ utest/test_translation.py
@@ -53,5 +53,6 @@ def test_keyword_names_are_unique(data: dict):
 
 
 def test_keyword_names_no_space(data: robotframework_browser_translation_fi.Language):
-    for translation in data:
+    for translation, value in data.items():
         assert " " not in translation, translation
+        assert " " not in value["name"]
