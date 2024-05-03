07b6425499d87891eae115fcac935f265de818e1
Tatu Aalto <aalto.tatu@gmail.com>
2024-05-03 22:05:15 +0300

    feat: added sha256 checksum to translation file
    Also updated required Browser library version and
    updated doc from Browser 18.4.0 release

--- utest/test_translation.py
+++ utest/test_translation.py
@@ -1,10 +1,21 @@
 import json
+import subprocess
+import sys
 from collections import Counter
 from pathlib import Path
 
 import pytest
 
 import robotframework_browser_translation_fi
+
+
+@pytest.fixture(scope="module")
+def file() -> Path:
+    return (
+        Path(__file__).parent.parent
+        / "robotframework_browser_translation_fi"
+        / "translation.json"
+    )
 
 
 @pytest.fixture(scope="module")
@@ -15,16 +26,11 @@ def data() -> robotframework_browser_translation_fi.Language:
         return json.load(file)
 
 
-def test_translation():
+def test_translation(file: Path):
     lang = robotframework_browser_translation_fi.get_language()
-    expexted_path = (
-        Path(__file__).parent.parent
-        / "robotframework_browser_translation_fi"
-        / "translation.json"
-    )
     assert lang["language"] == "fi"
     result_path = Path(lang["path"])
-    assert result_path == expexted_path
+    assert result_path == file
     assert result_path.is_file()
 
 
@@ -56,3 +62,21 @@ def test_keyword_names_no_space(data: robotframework_browser_translation_fi.Lang
     for translation, value in data.items():
         assert " " not in translation, translation
         assert " " not in value["name"], value
+
+
+def test_verify_checksum(file: Path, tmp_path: Path):
+    translation_file = tmp_path / "translation.json"
+    subprocess.run(
+        [sys.executable, "-m", "Browser.entry", "translation", translation_file],
+        check=True,
+    )
+    with translation_file.open("r") as source_translation:
+        source_data = json.load(source_translation)
+    with file.open("r") as translation_file:
+        translation_data = json.load(translation_file)
+    for kw in source_data:
+        source_sha256 = source_data[kw]["sha256"]
+        translation_sha256 = translation_data[kw]["sha256"]
+        assert (
+            source_sha256 == translation_sha256
+        ), f"{kw} sha256 was {source_sha256} expected {translation_sha256}"
